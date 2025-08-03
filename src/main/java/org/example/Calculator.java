b1b62966bb357d7296cdb7e88e8ddf7853dabf2b
Awat <awatdeu@gmail.com>
2025-08-03 16:39:18 +0200

    New Update

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double num1, num2;
+        char operator;
+        double result;
+
+        System.out.print("Geben Sie die erste Zahl ein: ");
+        num1 = input.nextDouble();
+
+        System.out.print("Geben Sie die zweite Zahl ein: ");
+        num2 = input.nextDouble();
+
+        System.out.print("Wählen Sie einen der Operatoren aus (+, -, *, /): ");
+        operator = input.next().charAt(0);
+
+        switch (operator) {
+
+            case '+':
+                result = num1 + num2;
+                System.out.println(num1 + " + " + num2 + " = " + result);
+                break;
+
+            case '-':
+                result = num1 - num2;
+                System.out.println(num1 + " - " + num2 + " = " + result);
+                break;
+
+            case '*':
+                result = num1 * num2;
+                System.out.println(num1 + " * " + num2 + " = " + result);
+                break;
+
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println(num1 + " / " + num2 + " = " + result);
+                } else {
+                    System.out.println("Fehler!!! Eine Division durch Null ist nicht möglich");
+                }
+                break;
+
+            default:
+                System.out.println("Fehler !!! Der eingegebene Operator ist ungültig. ");
+                break;
+        }
+
+        input.close();
+    }
+}
