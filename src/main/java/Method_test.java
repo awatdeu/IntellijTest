859dcc686fcf6d384463c6be476ce7e3d64c0e2b
Awat <awatdeu@gmail.com>
2025-08-08 13:17:20 +0200

    Update

---
+++ src/main/java/Method_test.java
@@ -0,0 +1,12 @@
+public class Method_test {
+    public static void main(String[] args) {
+
+    sum(22,19);
+
+    }
+
+    public static void sum(int fNumber,int sNumber){
+        int sum=fNumber+sNumber;
+        System.out.println("Sum 2 numbers: "+sum);
+    }
+}
