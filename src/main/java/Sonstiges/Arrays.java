41470836ad1f5d27ece8e06828d88f65b302c0a0
Awat <awatdeu@gmail.com>
2025-08-26 11:29:28 +0200

    Update

--- src/main/java/Sonstiges/Arrays.java
+++ src/main/java/Sonstiges/Arrays.java
@@ -1,25 +1,36 @@
 package Sonstiges;
 
+import java.time.LocalDateTime;
+
 public class Arrays {
     public static void main(String[] args) {
-    String [] autos= new String[6];
-        autos[0]= "BMW";
-        autos[1]= "Benz";
-//        System.out.println(autos[1]);
-        autos[2]= "Audi";
-        autos[3]= "VolksWagen";
-        autos[4]= "Porche";
-        autos[5]= "Skoda";
-        System.out.println(autos.length);
+
+//    String [] autos= new String[6];
+//        autos[0]= "BMW";
+//        autos[1]= "Benz";
+//      System.out.println(autos[1]);
+//        autos[2]= "Audi";
+//        autos[3]= "VolksWagen";
+//        autos[4]= "Porche";
+//        autos[5]= "Skoda";
+//        System.out.println(autos.length);
 
 //        for (int i=0;i< autos.length;i++){
 //            System.out.println(autos[i]);
-//        }
 
 //        for (String auto:autos){
 //            System.out.println(auto);
 //
 //        }
+        //-------------------------------------------------
+//        for(int i=1; i <= 5;i++){
+//            for(int j=0; j <= 5;j++){
+//            System.out.print(i);
+//        }
+//            System.out.println("  Result:  ".trim());
+//        }
+        //----------------------------------------------------
 
     }
+
 }
--- src/main/java/Sonstiges/Test.java
+++ src/main/java/Sonstiges/Test.java
@@ -1,18 +1,31 @@
 package Sonstiges;
 
+import java.util.Arrays;
+
 public class Test {
 
     public static void main(String[] args) {
 
-        int hoursOfDay=12;
-        int count = 0;
-
-
-        if (hoursOfDay<11)
-        {
-            System.out.println("AM");
-        count++;
+//        int hoursOfDay = 12;
+//        int count = 0;
+//
+//        if (hoursOfDay < 11) {
+//            System.out.println("AM");
+//            count++;
+//        }
+//        System.out.println("Count" + count);
+//
+//        int a [] = new int[5];
+//        a[0]=20;
+//        a[1]=6;
+//        a[2]=21;
+//        a[3]=30;
+//        a[4]=5;
+//        System.out.println(a);
+        int [] a={12,10,1,2,88,14,12,9,21,51,54,41,41,12};
+        Arrays.sort(a);
+        for(int i=0; i<a.length;i++){
+            System.out.println(a[i]);
         }
-        System.out.println("Count" + count);
     }
 }
