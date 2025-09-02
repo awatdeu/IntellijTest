cbbad176904d08efd84b538f890d3319d1b1ac2d
Awat <awatdeu@gmail.com>
2025-09-02 21:51:59 +0200

    Update

---
+++ src/main/java/Banking/Main.java
@@ -0,0 +1,26 @@
+package Banking;
+
+public class Main {
+    public static void main(String[] args) {
+
+        first();
+
+    }
+
+    public static void first(){
+        System.out.println("first called");
+        secound();
+
+    }
+    public static void secound(){
+
+        System.out.println("secound called");
+        third();
+
+    }
+    public static void third(){
+        System.out.println("third called");
+    }
+
+
+}
