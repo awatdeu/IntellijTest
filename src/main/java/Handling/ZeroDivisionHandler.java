f8cac67fb71369889e893cc4df704d9a2d6a01cd
Awat <awatdeu@gmail.com>
2025-09-14 00:07:10 +0200

    Update

---
+++ src/main/java/Handling/ZeroDivisionHandler.java
@@ -0,0 +1,28 @@
+package Handling;
+
+import javax.sound.midi.Soundbank;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class ZeroDivisionHandler {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        try {
+            System.out.println("عدد اول را وارد کنید : ");
+            int numerator= scanner.nextInt();
+            System.out.println("عدد دوم را وارد کنید :  ");
+            int denominator = scanner.nextInt();
+            int result = numerator / denominator;
+
+            System.out.println("نتیجه: " + result);
+
+        } catch (ArithmeticException e) {
+            System.out.println("خطا: تقسیم بر صفر مجاز نیست!");
+            System.out.println("لطفاً یک عدد غیر از صفر را به عنوان مقسوم علیه وارد کنید.");
+        }
+
+        System.out.println("\nبرنامه به پایان رسید و به درستی مدیریت شد.");
+    }
+}
---
+++ src/main/java/Test1/Main.java
@@ -0,0 +1,8 @@
+package Test1;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
