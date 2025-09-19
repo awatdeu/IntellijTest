f0dfc8c271fbdf060d34c0e019ded7e42506645b
Awat <awatdeu@gmail.com>
2025-09-19 23:31:54 +0200

    Update

--- src/main/java/Handling/ZeroDivisionHandler.java
+++ src/main/java/Handling/ZeroDivisionHandler.java
@@ -4,7 +4,7 @@ import javax.sound.midi.Soundbank;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class ZeroDivisionHandler {
+public class  ZeroDivisionHandler {
 
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
